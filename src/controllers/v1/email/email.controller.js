e821ee412f7124e3ea3b783ab6427db41fd929ec
spksoft NA <spkrsk.37@gmail.com>
2019-01-08 03:37:13 +0700

    add validator

--- src/controllers/v1/email/email.controller.js
+++ src/controllers/v1/email/email.controller.js
@@ -1,9 +1,25 @@
 import { HttpMethod, route } from '@spksoft/koa-decorator'
+import * as yup from 'yup'
+import validate from '@spksoft/koa-validator-decorator'
 import { send, search } from './email.flow'
 
 @route('/v1/email')
 class Email {
   @route('/send', HttpMethod.POST)
+  @validate({
+    body: yup.object().shape({
+      to: yup
+        .string()
+        .email()
+        .required(),
+      from: yup
+        .string()
+        .email()
+        .required(),
+      subject: yup.string().required(),
+      text: yup.string().required()
+    })
+  })
   async send(ctx) {
     const { to, from, subject, text, html } = ctx.request.body
     await send({ to, from, subject, text, html })
